f43431885c3e007eb020f552cfe2cc3e44ec0dad
logongas <lorenzo.profesor@gmail.com>
2014-05-21 09:17:47 +0200

    Cambiar la variable de entorno para configurar newrelic

--- .openshift/action_hooks/newrelic/newrelic.sh
+++ .openshift/action_hooks/newrelic/newrelic.sh
@@ -5,5 +5,5 @@ CONFIG_FILE=./newrelic.yml
 rm -f ${CONFIG_FILE}
 erb ${CONFIG_FILE}.erb > ${CONFIG_FILE}
 
-export CATALINA_OPTS="${CATALINA_OPTS} -javaagent:${OPENSHIFT_REPO_DIR}/.openshift/action_hooks/newrelic/newrelic.jar"
+export JAVA_TOOL_OPTIONS="${JAVA_TOOL_OPTIONS} -javaagent:${OPENSHIFT_REPO_DIR}/.openshift/action_hooks/newrelic/newrelic.jar"
 
